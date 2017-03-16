351ca241644f2f76f73bd25725a4fa5ce7180001
tomco <tom.cools@live.be>
2017-03-16 16:28:48 +0100

    Removed code that should have been removed before.

--- src/main/java/com/infosupport/ap/exercise/resources/ImageResource.java
+++ src/main/java/com/infosupport/ap/exercise/resources/ImageResource.java
@@ -3,16 +3,11 @@ package com.infosupport.ap.exercise.resources;
 import com.infosupport.ap.exercise.models.Presence;
 import com.infosupport.ap.exercise.models.RegistrationImage;
 import com.infosupport.ap.exercise.services.CognitiveServices;
-import com.infosupport.ap.exercise.services.responses.DetectFaces;
-import com.infosupport.ap.exercise.services.responses.GetPerson;
-import com.infosupport.ap.exercise.services.responses.Identification;
-import com.infosupport.ap.exercise.services.responses.partial.IdentificationCandidates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.BASE64Decoder;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 @RestController
 public class ImageResource {
@@ -28,10 +23,8 @@ public class ImageResource {
         byte[] imageByte = decoder.decodeBuffer(body.getImage());
 
         //TODO Task 3B - Detecting person in image
-        DetectFaces detectFace = faceAPI.detect(imageByte);
-        Identification identification = faceAPI.identify(Arrays.asList(detectFace.getFaceId()));
-        IdentificationCandidates candidate = identification.getCandidates().get(0);
-        GetPerson person = faceAPI.getPerson(candidate.getPersonId());
+
+        //END - TODO TASK 3B
 
         //TODO Task 4: create presence and repository, save and return
         return new Presence();
